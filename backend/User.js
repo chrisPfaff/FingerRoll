a20910f3d6b6a42ad6d5ce6bc2c3930d50b700f3
Chris Pfaff <chrispfaff10@gmail.com>
2018-12-13 17:20:48 -0600

    changed schema tohave required attr

--- backend/User.js
+++ backend/User.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-  name: String,
-  team: String
+  name: {
+    type: String,
+    required: true
+  },
+  team: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model("User", User);
