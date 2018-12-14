7d13e38a1bcc2f901c8c2755216d9437850151ff
Chris Pfaff <chrispfaff10@gmail.com>
2018-12-13 19:39:27 -0600

    added componentDidMount to test if i can fetch data from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    fetch("/api/data")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data.data[0]);
+        this.setState({ data: data.data[0] });
+      });
+  }
   render() {
-    return <div>hello world</div>;
+    return (
+      <div>
+        <p>{this.state.data.name}</p>
+        <p>{this.state.data.team}</p>
+      </div>
+    );
   }
 }
 
