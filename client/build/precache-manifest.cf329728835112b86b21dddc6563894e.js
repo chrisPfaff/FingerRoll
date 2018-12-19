73df48851b87620a71f20fb049a5b65f458eabb2
Chris Pfaff <chrispfaff10@gmail.com>
2018-12-19 11:22:39 -0600

    added image to dir

---
+++ client/build/precache-manifest.cf329728835112b86b21dddc6563894e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5b83cc2d043c5af7f7f1c7fc3773666",
+    "url": "/static/media/hokjesgeest-webfont.f5b83cc2.woff2"
+  },
+  {
+    "revision": "127935f4d710bb7e4a8adcb87121c188",
+    "url": "/static/media/hokjesgeest-webfont.127935f4.woff"
+  },
+  {
+    "revision": "110906f825f55474610f4317239456f7",
+    "url": "/static/media/basketball.110906f8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee8ba482ed5c956503c4",
+    "url": "/static/js/main.ee8ba482.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "ee8ba482ed5c956503c4",
+    "url": "/static/css/main.43de78cf.chunk.css"
+  },
+  {
+    "revision": "bed92fce39304fcafae80e28a65989e5",
+    "url": "/index.html"
+  }
+];
